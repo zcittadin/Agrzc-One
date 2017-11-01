cf33c8e559f18fcbc13b47011832ac3e465219d2
zcittadin <z_cittadin@hotmail.com>
2017-11-01 07:57:21 -0200

    Commit inicial

---
+++ src/main/java/com/servicos/estatica/stage/one/app/AgrzcStageOne.java
@@ -0,0 +1,17 @@
+package com.servicos.estatica.stage.one.app;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AgrzcStageOne extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+
+	}
+
+	public static void main(String[] args) {
+		launch();
+	}
+
+}
